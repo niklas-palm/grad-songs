26fa02705283035b6176a8390f8448cb75e40fe8
Niklas Palm <nipalm@amazon.com>
2021-09-03 11:39:47 +0200

    Add first song

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
-function App() {
+import songs from "./songs.js";
+
+const songItem = (song) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div key={song.title} className="songItem">
+      <div className="songHeader">
+        <h1 style={{ marginBottom: "0.1em" }}>{song.title}</h1>
+        <h3 style={{ marginBottom: 0 }}>{song.author}</h3>
+        <p style={{ marginTop: "0.3em" }}> Melodi: {song.original}</p>
+      </div>
+      <div className="songText">{song.text}</div>
     </div>
   );
-}
+};
+
+const renderSongItems = () => {
+  return songs.map((song) => songItem(song));
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="header">This is a header</div>
+      {renderSongItems()}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/songs.js
@@ -0,0 +1,45 @@
+const songs = [
+  {
+    title: "Examensfest",
+    author: "Adam Linde, Niklas Palm",
+    original: "Bella Ciao",
+    text: `
+      Gick upp en morgon 
+      Blev genast taggad
+      På STS, STS på examensfest
+      Gick upp en morgon 
+      Blev genast taggad 
+      På att få festa på nation
+
+      Kom till nationen
+      Blev mera taggad
+      På STS, STS på examensfest
+      Kom till nationen
+      Blev mera taggad
+      På att få dricka massa sprit
+
+      Vi drack up spriten
+      Blev jättetaggad
+      På STS, STS på examensfest
+      Vi drack up spriten
+      Blev jättetaggad
+      På att få dansa natten lång
+
+      När vi har dansat
+      Blir det Palermo
+      På efterfest efterfest på examensfest
+      När vi har dansat
+      Blir det Palermo
+      Och sen så vaknar vi igen
+
+      Gick upp en morgon 
+      Blev genast taggad
+      På STS, STS på examensfest
+      Gick upp en morgon 
+      Blev genast taggad 
+      På att få festa på nation"
+      `,
+  },
+];
+
+export default songs;
